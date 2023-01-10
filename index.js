facf6606820160c75395078059e5c533e25ed4fc
malitharuna <msmalitharuna@gmail.com>
2023-01-10 15:49:54 +0600

    vercel add

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 
 const categories = require('./data/categories.json')
-const course = require('')
+const course = require('./data/course.json')
+
 app.get('/', (req, res) => {
     res.send('server is running');
 });
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+
+app.get('/category/:id', (req, res) =>{
+    const id = req.params.id;
+    const courseNumber = course.find(c => c.category_id === id)
+    res.send(courseNumber)
 })
 
 app.get('/allCourse', (req, res) => {
